9dfb5a9c96bc9f43726992bf229a214913050d01
Dmither <mr.d.graumann@gmail.com>
2023-10-26 09:37:34 +0300

    fix connect spoilers js

--- js/main.js
+++ js/main.js
@@ -1,5 +1,3 @@
-import { InitSpoiler } from "./onlyOneSpoiler";
-
 let menu = document.querySelector(".header__menu");
 let burger = document.querySelector(".header__burger");
 let body = document.body;
@@ -11,11 +9,32 @@ burger.addEventListener("click", function(event) {
   body.classList.toggle("frozen")
 })
 
-let spoilersList = document.querySelectorAll(".connect__content .connect__item");
-console.log(spoilersList)
-InitSpoiler(spoilersList);
-
 let progresses = document.querySelectorAll(".connect__progress");
 progresses.forEach(progress => {
   progress.children[0].style.width = progress.dataset.fill + "%"
+});
+
+let spoilerBlock = document.querySelector(".connect__content");
+let spoilersList = document.querySelectorAll(".connect__content .connect__item");
+
+spoilersList.forEach(spoilerItem => {
+  console.log(spoilerItem)
+  if (!spoilerItem.classList.contains("_opened")) {
+    spoilerItem.children[1].style.height = 0 + "px";
+  } else {
+    spoilerItem.children[1].style.height = spoilerItem.children[1].children[0].offsetHeight + "px";
+  }
+  spoilerItem.addEventListener("click", function(event){
+    if (!spoilerItem.classList.contains("_opened")) {
+      spoilersList.forEach(item => {
+        item.classList.remove("_opened");
+        item.children[1].style.height = 0 + "px";
+      })
+      spoilerItem.classList.add("_opened");
+      spoilerItem.children[1].style.height = spoilerItem.children[1].children[0].offsetHeight + "px";
+    } else {
+      spoilerItem.classList.remove("_opened");
+      spoilerItem.children[1].style.height = 0 + "px";
+    }
+  })
 });
