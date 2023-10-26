231d1f14c1086a87224a4f9d196e77779044aaa5
Dmither <mr.d.graumann@gmail.com>
2023-10-26 10:49:42 +0300

    fix mobile menu with checkbox

--- js/main.js
+++ js/main.js
@@ -3,9 +3,6 @@ let burger = document.querySelector(".header__burger");
 let body = document.body;
 
 burger.addEventListener("click", function(event) {
-  console.log("burger click")
-  menu.classList.toggle("active")
-  burger.classList.toggle("active")
   body.classList.toggle("frozen")
 })
 
